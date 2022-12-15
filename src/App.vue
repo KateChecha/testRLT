95e2fc85b50ad5f35f28ee93352fe714d228aa0c
Катерина Чечёткина <katechecha@gmail.com>
2022-12-15 17:07:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+  import LootFieldVue from './components/LootField.vue';
+  export default{
+    components: { LootFieldVue }
+  }
+</script>
+
+<template>
+  <LootFieldVue/>
+</template>
+
+<style scoped>
+
+</style>
